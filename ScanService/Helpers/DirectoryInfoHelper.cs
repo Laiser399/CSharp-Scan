ccf31437c531fac7599c569393c39ca09f11ef40
Laiser399 <Laiser399@mail.ru>
2021-10-31 13:54:23 +0300

    забытый хелпер

---
+++ ScanService/Helpers/DirectoryInfoHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Security;
+
+namespace ScanService.Helpers
+{
+    public static class DirectoryInfoHelper
+    {
+        public static bool TryGetFiles(this DirectoryInfo directory, out FileInfo[] files)
+        {
+            try
+            {
+                files = directory.GetFiles();
+                return true;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                files = Array.Empty<FileInfo>();
+                return false;
+            }
+        }
+        
+        public static bool TryGetDirectories(this DirectoryInfo directory, out DirectoryInfo[] files)
+        {
+            try
+            {
+                files = directory.GetDirectories();
+                return true;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                files = Array.Empty<DirectoryInfo>();
+                return false;
+            }
+            catch (SecurityException)
+            {
+                files = Array.Empty<DirectoryInfo>();
+                return false;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                files = Array.Empty<DirectoryInfo>();
+                return false;
+            }
+        }
+    }
+}
